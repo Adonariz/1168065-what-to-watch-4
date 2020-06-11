c440d2fb53c08052ae47c3fefd218aed927bbf37
A Zhila <aleksej.zhila@gmail.com>
2020-06-11 17:35:46 +0300

    Добавляет props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {title, genre, year} = props;
+
   return (
-    <Main/>
+    <Main
+      title={title}
+      genre={genre}
+      year={year}
+    />
   );
 };
 
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,9 @@
-import React, {Fragment} from "react";
+import React from "react";
 
 const Main = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {title, genre, year} = props;
+
   return (
     <React.Fragment>
       <section className="movie-card">
@@ -33,10 +36,10 @@ const Main = (props) => {
             </div>
 
             <div className="movie-card__desc">
-              <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+              <h2 className="movie-card__title">{title}</h2>
               <p className="movie-card__meta">
-                <span className="movie-card__genre">Drama</span>
-                <span className="movie-card__year">2014</span>
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{year}</span>
               </p>
 
               <div className="movie-card__buttons">
