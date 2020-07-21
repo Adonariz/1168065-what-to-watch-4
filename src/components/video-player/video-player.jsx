abaf4142494bbdb12ffe5bd94811f9de03a18597
A Zhila <aleksej.zhila@gmail.com>
2020-07-21 23:02:14 +0300

    Добавляет компонент плеера и снепшот тест

---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,60 @@
+import React, {PureComponent, Fragment, createRef} from "react";
+import PropTypes from "prop-types";
+
+export default class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = createRef();
+
+    this.state = {
+      isPlaying: props.isPlaying,
+    };
+  }
+
+  render() {
+    const {poster} = this.props;
+
+    return (
+      <Fragment>
+        <video
+          className={`player__video`}
+          ref={this._videoRef}
+          poster={poster}
+          muted
+        />
+      </Fragment>
+    );
+  }
+
+  componentDidMount() {
+    const {src} = this.props;
+    const video = this._videoRef.current;
+
+    video.src = src;
+
+    video.onplay = () => {
+      this.setState({
+        isPlaying: true,
+      });
+    };
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+    video.onplay = null;
+    video.src = ``;
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+    video.onplay = null;
+    video.src = ``;
+  }
+}
+
+VideoPlayer.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+};
