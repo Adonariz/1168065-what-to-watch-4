ad28a53825f2fe405ea9839624cf9edaed310d37
A Zhila <aleksej.zhila@gmail.com>
2020-07-22 00:20:25 +0300

    Небольшие правки

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -39,7 +39,12 @@ class SmallMovieCard extends PureComponent {
         }}
       >
         <div className="small-movie-card__image">
-          <VideoPlayer isPlaying={isPlaying} src={movie.src} poster={movie.image} muted/>
+          <VideoPlayer
+            isPlaying={isPlaying}
+            src={movie.src}
+            poster={movie.image}
+            muted
+          />
           <img src={movie.image} alt={movie.title} width="280" height="175" />
         </div>
         <h3
