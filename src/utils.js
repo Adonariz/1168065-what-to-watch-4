8b906ed3b39188188d924b1df6f6045740c03847
A Zhila <aleksej.zhila@gmail.com>
2020-07-28 20:16:13 +0300

    Проверка тревиса

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export const formatReviewDate = (dateString) => {
   const date = new Date(dateString);
-  date.setDate(date.getDate() + 1);
+  date.setUTCDate(date.getDate());
 
   return date.toISOString().slice(0, 10);
 };
